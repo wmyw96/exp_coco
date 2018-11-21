356d4576dc5b394ff0a5ddf0eecf01cb42804de4
wmyw96 <wmyw96@126.com>
2018-11-22 03:41:02 +0800

    init

---
+++ coco_model.py
@@ -0,0 +1,229 @@
+import tensorflow as tf
+
+
+tf.set_random_seed(123456)
+
+
+def show_params(name, params):
+    print("Training Parameters for {}".format(name))
+    for para in params:
+        print(para.name + ': ' + str(para.get_shape()))
+
+
+def embed_norm(x, axes):
+    norm2 = tf.reduce_sum(tf.square(x), axes, keep_dims=True)
+    norm = tf.sqrt(norm2) + 1e-4
+    return x / norm
+
+
+class UniMappingGAN:
+
+    def __init__(self, s, seq_len, width, height, nlabels=10, channels=3,
+                 batch_size=8, markov=1, g_lr=1e-4,
+                 d_lr=1e-4, epsilon=1e-9):
+        self.s = s
+        self.batch_size = batch_size
+
+        self.seq_len = seq_len
+        self.width = width
+        self.height = height
+        self.nlabels = nlabels
+        self.channels = channels
+
+        self.beta1 = 0.5
+        self.beta2 = .999
+        self.d_lr = d_lr
+        self.g_lr = g_lr
+        self.lambda_ = 10.
+        self.lambda_rec = 10.
+        self.lambda_cycle = 10.
+        self.n_train_critic = 10
+        self.eps = epsilon
+        self.n_experts = 5
+        self.weight_decay = 0.01
+        self.lambda_gp_g = 0.01
+        self.n_concats = markov
+
+        self.w_b = None
+        self.w = None
+
+        self.gp_b = None
+        self.gp = None
+        self.gs_op = None
+        self.e_op = None
+
+        self.g_loss = 0.
+        self.g_b_loss = 0.
+        self.d_loss = 0.
+        
+        self.d_op = None
+        self.g_op = None
+        self.vars = []
+        self.merged = None
+        self.writer = None
+        self.saver = None
+        self.use_embed = True
+        self.pred, self.accuracy = None, None
+        self.embed_dim = 128
+
+        self.g_super_loss = 0.
+
+        # placeholders
+        self.image = \
+            tf.placeholder(tf.float32,
+                           [None, self.height, self.width,
+                            self.channels], name='image')
+        self.label = \
+            tf.placeholder(tf.float32,
+                           [None, self.nlabels], name='label')
+        self.olabel = \
+            tf.placeholder(tf.float32,
+                           [None, self.nlabels], name='oracle-label')
+        self.lr_decay = tf.placeholder(tf.float32, shape=(),
+                                       name='learning_rate-decay')
+        self.is_training = tf.placeholder(tf.bool, shape=[], name='is_training')
+        self.n_decay = tf.placeholder(tf.float32, shape=(), name='noise_decay')
+        self.build_cyclegan()  # build CycleGAN
+
+    def discriminator(self, x, reuse=None, name=""):
+        with tf.variable_scope('discriminator-%s' % name, reuse=reuse):
+            hidden = 512
+            out = tf.layers.dense(x, hidden, activation=tf.nn.relu,
+                                  name=name + '.1', reuse=reuse)
+            out = tf.layers.dense(out, hidden, activation=tf.nn.relu,
+                                  name=name + '.2', reuse=reuse)
+            out = tf.layers.dense(out, hidden, activation=tf.nn.relu,
+                                  name=name + '.3', reuse=reuse)
+            out = tf.layers.dense(out, 1, activation=None,
+                                  name=name + '.out', reuse=reuse)
+            return out
+
+    def generator(self, x, reuse=None, name=""):
+        if reuse is None:
+            reuse = False
+        with tf.variable_scope('generator-%s' % name, reuse=reuse):
+            is_training = self.is_training
+            ndf = 64
+            lc_x = tf.layers.conv2d(x, ndf, 4, strides=(2, 2),
+                                    padding='same', use_bias=False)
+            lc_x = tf.nn.relu(lc_x)
+            lc_x = tf.layers.conv2d(lc_x, ndf * 2, 4, strides=(2, 2),
+                                    padding='same', use_bias=False)
+            lc_x = tf.nn.relu(lc_x)
+            lc_x = tf.layers.conv2d(lc_x, ndf * 4, 4, strides=(2, 2),
+                                    padding='same', use_bias=False)
+            lc_x = tf.nn.relu(lc_x)
+            lc_x = tf.layers.conv2d(lc_x, ndf * 8, 4, strides=(2, 2),
+                                    padding='same', use_bias=False)
+            lc_x = tf.layers.conv2d(lc_x, ndf * 8, 4, use_bias=False)
+            lc_x = tf.nn.relu(lc_x)
+
+            print(lc_x.get_shape())
+            lc_x = tf.reshape(lc_x, [-1, ndf * 8 * 5 * 5])
+
+            out = tf.layers.dense(lc_x, self.embed_dim, activation=tf.nn.tanh)
+            return out
+
+
+    def markov(self, x):
+        return tf.reshape(x, [self.seq_len // self.n_concats, self.n_concats * self.embed_dim])
+
+    def markov_interpolate(self, real, fake):
+        real_sp = tf.reshape(real, [self.seq_len // self.n_concats, self.n_concats, self.embed_dim])
+        fake_sp = tf.reshape(fake, [self.seq_len // self.n_concats, self.n_concats, self.embed_dim])
+        alpha = tf.random_uniform([self.seq_len // self.n_concats, self.n_concats, 1], 0, 1, seed=123456)
+        out = real_sp * alpha + (1 - alpha) * fake_sp
+        print('M_I' + str(out.get_shape()))
+        return self.markov(out)
+
+    def build_cyclegan(self):
+        # Generator
+        self.a = tf.identity(self.image)
+        with tf.variable_scope("generator-img2eb"):
+            self.a2b = self.generator(self.a, name='g_img2lb')
+
+        with tf.variable_scope("label2embed"):
+            self.embeddings = tf.get_variable('label-embed',
+                                              [self.nlabels, self.embed_dim])
+            self.embeddings = embed_norm(self.embeddings, 1)
+            self.inner_product = tf.matmul(self.embeddings, self.embeddings,
+                                           transpose_b=True)
+            self.loss_inner_product = tf.reduce_sum(tf.square(self.inner_product))
+            noise = tf.random_normal([self.seq_len, self.embed_dim], 0.0, 0.04 * self.n_decay, seed=123456)
+            self.b = tf.matmul(self.label, self.embeddings) + noise
+            self.ora_b = tf.matmul(self.olabel, self.embeddings) + noise
+
+        self.g_super_loss = tf.reduce_mean(tf.square(self.a2b - self.ora_b))
+        self.g_ssuper_loss = tf.reduce_mean(tf.square(self.a2b - self.ora_b))
+
+        logits = tf.matmul(self.a2b, self.embeddings,
+                           transpose_b=True)
+        self.pred = tf.one_hot(tf.argmax(logits, 1), self.nlabels)
+        ncorrect = (tf.reduce_sum(self.pred * self.olabel) + 0.0)
+        self.accuracy = ncorrect / self.seq_len
+
+        # Classifier
+        with tf.variable_scope("discriminator-b"):
+            d_b = self.discriminator(self.markov(self.b))
+            d_a2b = self.discriminator(self.markov(self.a2b),
+                                       reuse=True)
+            b_hat = self.markov_interpolate(self.b,
+                                            self.a2b)
+            d_b_hat = self.discriminator(b_hat, reuse=True)
+
+        # Training Ops
+        self.w_b = tf.reduce_mean(d_b) - tf.reduce_mean(d_a2b)
+
+        self.w = self.w_b
+
+        self.gp_b = tf.reduce_mean(
+            (tf.sqrt(tf.reduce_sum(tf.gradients(d_b_hat, b_hat)[0] ** 2,
+                                   reduction_indices=[1])) - 1.) ** 2
+        )
+        self.gp = self.gp_b
+
+        self.d_loss = self.lambda_ * self.gp - self.w
+
+        # using adv loss
+        self.g_b_loss = -1. * tf.reduce_mean(d_a2b)
+        self.g_loss = self.g_b_loss
+
+        # Optimizer
+        t_vars = tf.trainable_variables()
+        show_params('Full', t_vars)
+
+        d_params = [v for v in t_vars if v.name.startswith('discriminator')]
+        g_params = [v for v in t_vars if v.name.startswith('generator')]
+        e_params = [v for v in t_vars if v.name.startswith('label2embed')]
+
+        show_params('Discriminator', d_params)
+        show_params('Generator', g_params)
+        show_params('Embedding', e_params)
+
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            self.d_op = \
+                tf.train.AdamOptimizer(learning_rate=self.d_lr * self.lr_decay,
+                                       beta1=self.beta1, beta2=self.beta2).\
+                minimize(self.d_loss, var_list=d_params)
+            self.g_op = \
+                tf.train.AdamOptimizer(learning_rate=self.g_lr * self.lr_decay,
+                                       beta1=self.beta1, beta2=self.beta2).\
+                minimize(self.g_loss, var_list=g_params)
+
+            self.gs_op = \
+                tf.train.AdamOptimizer(learning_rate=self.g_lr * self.lr_decay,
+                                       beta1=self.beta1, beta2=self.beta2).\
+                minimize(self.g_ssuper_loss, var_list=g_params)
+        self.e_op = \
+                tf.train.AdamOptimizer(learning_rate=self.g_lr * self.lr_decay,
+                                       beta1=self.beta1, beta2=self.beta2).\
+                minimize(self.loss_inner_product, var_list=e_params)
+
+        # Merge summary
+        self.merged = tf.summary.merge_all()
+
+        # Model saver
+        self.saver = tf.train.Saver(max_to_keep=1)
+        self.writer = tf.summary.FileWriter('./model/', self.s.graph)
+
---
+++ main_coco.py
@@ -0,0 +1,308 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import os
+import tensorflow as tf
+import numpy as np
+import time
+import coco_model
+import itertools
+from skimage import io, data
+import argparse
+
+
+try:
+    import matplotlib.pyplot as plt
+except:
+    print('Cannot import matplotlib')
+
+
+parser = argparse.ArgumentParser(description='Argument parser')
+
+parser.add_argument('--seed', dest='seed', type=int, default=123456,
+                    help='random seed')
+
+
+def KM_match(transformed, ground_truth):
+    from KM import min_KM
+    weight = np.zeros((transformed.shape[0], transformed.shape[0]))
+    for i in range(weight.shape[0]):
+        for j in range(weight.shape[1]):
+            weight[i, j] = np.mean(np.square(transformed[i, :] - ground_truth[j, :]))
+    KMdist, cor = min_KM(weight)
+    KMdist /= weight.shape[0]
+    return KMdist, cor
+
+
+args = parser.parse_args()
+np.random.seed(args.seed)
+nlabels = 5
+
+# load dataset
+dataset_path = 'data/'
+docs = []
+files = os.listdir(dataset_path)
+
+corpus = []
+#corpus_size = 4000
+corpus_size = 3000
+
+for i in range(corpus_size):
+    corpus.append([])
+
+joint_prob = np.zeros((nlabels, nlabels))
+npairs = 0
+nsel = 0
+indexes = []
+
+for fi in files:
+    if fi[-3:] != 'png':
+        continue
+    fi_d = os.path.join(dataset_path, fi)
+    img_id, cat_id, in_id = fi.split('.')[0].split('_')
+
+    img_id = int(img_id[3:])
+    cat_id = int(cat_id)
+    in_id = int(in_id)
+
+    if img_id >= corpus_size:
+        continue
+
+    image = io.imread(fi_d)
+    corpus[img_id].append((image, cat_id))
+    if len(corpus[img_id]) == 2:
+        nsel += 1
+
+for i in range(corpus_size):
+    if len(corpus[i]) <= 1:
+        pass
+    else:
+        cc = len(corpus[i]) * (len(corpus[i]) - 1)
+        for c1 in range(len(corpus[i])):
+            for c2 in range(c1):
+                joint_prob[corpus[i][c1][1], corpus[i][c2][1]] += 1.0 / cc / nsel
+                joint_prob[corpus[i][c2][1], corpus[i][c1][1]] += 1.0 / cc / nsel
+
+print('Joint Prob Matrix')
+print(joint_prob)
+print(np.sum(joint_prob))
+
+
+train_step = {
+    'epochs': 21,
+    'test_logging_step': 100,
+    'logging_step': 100,
+    'seq_len': 200,
+    'markov': 2,
+    'embed': 128
+}
+
+
+# pair-wise data
+def sample_image_seq(seq_len=10):
+    choice = [i for i in range(nlabels)]
+    horizon = seq_len
+    image = np.zeros((horizon, 128, 128, 3))
+    label = np.zeros((horizon, nlabels))
+    for i in range(seq_len // 2):
+        img_id = np.random.randint(0, corpus_size)
+        while (len(corpus[img_id]) <= 1):
+            img_id = np.random.randint(0, corpus_size)
+
+        l_id = np.random.randint(0, len(corpus[img_id]))
+        r_id = np.random.randint(0, len(corpus[img_id]))
+        while r_id == l_id:
+            r_id = np.random.randint(0, len(corpus[img_id]))
+        image[2 * i, :, :, :] = corpus[img_id][l_id][0]
+        label[2 * i, corpus[img_id][l_id][1]] = 1.0
+        image[2 * i + 1, :, :, :] = corpus[img_id][r_id][0]
+        label[2 * i + 1, corpus[img_id][r_id][1]] = 1.0
+    return image, label
+
+
+def main():
+    # GPU configure
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    path = 'coco/'
+    if os.path.isdir(path):
+        pass
+    else:
+        print('build new direction ...')
+        os.mkdir(path)
+
+    tf.set_random_seed(123456)
+    with tf.Session(config=config) as s:
+        # CycleGAN Model
+        #tf.set_random_seed(123456)
+        n_concat = 2
+
+        model = \
+            coco_model.UniMappingGAN(s,
+                                      seq_len=train_step['seq_len'],
+                                      width=128, height=128, nlabels=nlabels,
+                                      markov=n_concat)
+
+        # Initializing
+        s.run(tf.global_variables_initializer())
+        global_step = 0
+
+        mses = []
+        for my_iter in range(1000):
+            _, pd, kk = s.run([model.e_op, model.loss_inner_product,
+                              model.inner_product],
+                              feed_dict={model.lr_decay: 1.0})
+        for epoch in range(train_step['epochs']):
+            # learning rate decay
+            lr_decay = 1.
+            if epoch >= 100 and epoch % 10 == 0:
+                lr_decay = (train_step['epochs'] - epoch) / (
+                    train_step['epochs'] / 2.)
+            
+            for i in range(1000):
+                n_decay = 0.5 * max((20000 - global_step + 0.0) / 20000, 0) + 0.5
+
+                for _ in range(model.n_train_critic):
+                    a_img, a_label = sample_image_seq(train_step['seq_len'])
+                    b_img, b_label = sample_image_seq(train_step['seq_len'])
+                    _ = s.run(model.d_op,
+                              feed_dict={model.image: a_img,
+                                         model.label: b_label,
+                                         model.is_training: True,
+                                         model.lr_decay: lr_decay,
+                                         model.n_decay: n_decay})
+
+                a_img, a_label = sample_image_seq(train_step['seq_len'])
+                b_img, b_label = sample_image_seq(train_step['seq_len'])
+
+                w, gp, g_loss, acc, _, mse = \
+                    s.run([model.w, model.gp, model.g_loss, model.accuracy,
+                           model.g_op, model.g_super_loss],
+                          feed_dict={model.image: a_img, model.label: b_label,
+                                     model.olabel: a_label,
+                                     model.is_training: True,
+                                     model.lr_decay: lr_decay,
+                                     model.n_decay: n_decay})
+                mses.append(mse)
+
+                if global_step % train_step['logging_step'] == 0:
+                    # Print loss
+                    tr, gt, pred, bd = s.run([model.a2b, model.b, model.pred, model.embeddings],
+                                   feed_dict={model.image: a_img,
+                                              model.label: a_label,
+                                              model.is_training: False, model.n_decay: n_decay})
+                    pred = np.argmax(pred, 1)
+                    kmdist, _ = KM_match(tr, gt)
+                    print("[+] Global Step %08d =>" % global_step,
+                          " G loss     : {:.8f}".format(g_loss),
+                          " w          : {:.8f}".format(w),
+                          " gp         : {:.8f}".format(gp),
+                          " Accuracy   : {:.8f}".format(acc),
+                          " L2 norm    : {:.8f}".format(mse),
+                          " KM dist    : {:.8f}".format(kmdist))
+                    mses = []
+
+                global_step += 1
+
+            d_a, d_a2b = [], []
+
+            if args.model == 'nodop':
+                continue
+            
+            print('[+] Supervised Shuffling, A->B ...')
+
+            horizon = train_step['seq_len']
+            myjoint = np.zeros((nlabels, nlabels))
+            mycnt = 0.0
+
+            cm = np.zeros((nlabels, nlabels))
+            for u in range(5000):
+                u_gta, u_ = sample_image_seq(horizon)
+                u_gt = np.argmax(u_, 1)
+                u_pd = s.run(model.pred, feed_dict={model.image: u_gta,
+                                                    model.is_training: False})
+                u_fs = np.argmax(u_pd, 1)
+                for k in range(horizon // 2):
+                    if (k + 1) % train_step['markov'] != 0:
+                        myjoint[u_fs[k * 2], u_fs[k * 2 + 1]] += 1.0
+                        myjoint[u_fs[k * 2 + 1], u_fs[k * 2]] += 1.0
+                        mycnt += 2.0
+                        cm[u_gt[k * 2], u_fs[k * 2]] += 1
+                        cm[u_gt[k * 2 + 1], u_fs[k * 2 + 1]] += 1
+
+            myjoint /= mycnt
+            print(np.sum(myjoint))
+
+            myweight = np.zeros((nlabels, nlabels))
+            for x in range(nlabels):
+                for y in range(nlabels):
+                    myweight[x, y] = joint_prob[y, x]
+
+            best_se = 1e9
+            reord_label = [0] * horizon
+            solution = []
+            for perm in itertools.permutations([k for k in range(nlabels)]):
+                cur_se = 0.
+                for x in range(nlabels):
+                    for y in range(nlabels):
+                        cur_se += np.abs(
+                            mytrans[x, y] - myweight[perm[x], perm[y]])
+                if cur_se < best_se:
+                    best_se = cur_se
+                    solution = [perm[k] for k in range(nlabels)]
+            print('Discrete Optimization Solution')
+            print(solution)
+
+            for miter in range(100):
+                gta, _ = sample_image_seq(horizon)
+                tb, pd, bd = s.run(
+                    [model.a2b, model.pred, model.embeddings],
+                    feed_dict={model.image: gta,
+                               model.is_training: False})
+                
+                pred_idx = np.argmax(pd, 1)
+                gt_idx = np.argmax(_, 1)
+
+                for k in range(horizon):
+                    reord_label[k] = solution[pred_idx[k]]
+
+                acc_o = sum([pred_idx[k] == gt_idx[k] for k in range(horizon)])
+                acc_r = sum([reord_label[k] == gt_idx[k]
+                             for k in range(horizon)])
+                if miter == 0:
+                    print('Origin Acc {}, Reordered Acc {}, '.
+                          format(acc_o, acc_r))
+
+                gtab = np.zeros((horizon, nlabels))
+                for k in range(horizon):
+                    gtab[k, reord_label[k]] = 1.0
+
+                d_a.append(gta)
+                d_a2b.append(gtab)
+
+            decay = 1.0
+            for k in range(2000):
+                idx = np.random.randint(100)
+                img, lb = sample_image_seq(train_step['seq_len'])
+                n_decay = max((20000 - global_step + 0.0) / 20000, 0) * 0.5 + 0.5
+                _, ls_ssp = \
+                    s.run([model.gs_op, model.g_super_loss],
+                          feed_dict={model.image: d_a[idx],
+                                     model.label: lb,
+                                     model.olabel: d_a2b[idx],
+                                     model.lr_decay: decay,
+                                     model.is_training: True,
+                                     model.n_decay: n_decay})
+                if k % 100 == 0:
+                    decay *= 0.8
+                if k % 100 == 0:
+                    print('[+] Supervised Shuffling, MSE Loss = {}'.
+                          format(ls_ssp))
+
+    # Close tf.Session
+    s.close()
+
+
+if __name__ == '__main__':
+    main()
